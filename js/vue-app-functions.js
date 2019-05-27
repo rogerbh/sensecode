b7989245ac0ee1da16fdc4b17eae7f431da9e719
Rogerio Huss <rogeriohuss@wareline.com.br>
2019-05-27 16:22:34 -0300

    rodape com vuejs

---
+++ js/vue-app-functions.js
@@ -0,0 +1,9 @@
+var vueFooterData = new Vue({
+	el: "#vueFooterData",
+    data: {
+        linkToVsCode: "<a href='https://code.visualstudio.com/' target='_blank'>VSCode</a>, ",
+        linkToJekyll: "<a href='https://jekyllrb.com/' target='_blank'>Jekyll</a>, ",
+        linkToBootstrap: "<a href='http://getbootstrap.com/' target='_blank'>Bootstrap 4</a> and more cool stuff!",
+        linkToGitHub: "<br>Kindly hosted by <a href='https://pages.github.com/' target='_blank'>GitHub Pages</a>"
+    }
+})
