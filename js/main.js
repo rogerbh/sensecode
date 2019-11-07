395b023e7ebc0b83e9c3bb43be353f495039f932
Rogério Huss <rogeriohuss@wareline.com.br>
2019-11-07 10:01:28 -0300

    prod version

--- js/main.js
+++ js/main.js
@@ -1,61 +1,62 @@
 // equivalente ao document.ready da biblioteca jQuery
 // in case the document is already rendered
-if (document.readyState != 'loading') {
-    randomPostWidth();
-    if (document.getElementById('svgToShowMe') !== null)
-        clickMeEvent();
+if (document.readyState != "loading") {
+  randomPostWidth();
+  if (document.getElementById("svgToShowMe") !== null) clickMeEvent();
+  // rescroll();
 }
 // modern browsers
 else if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', function () {
-        randomPostWidth();
-        if (document.getElementById('svgToShowMe') !== null)
-            clickMeEvent();
-    });
+  document.addEventListener("DOMContentLoaded", function() {
+    randomPostWidth();
+    if (document.getElementById("svgToShowMe") !== null) clickMeEvent();
+    // rescroll();
+  });
 }
 // FIM equivalente ao document.ready da biblioteca jQuery
 
 function randomPostWidth() {
-    const postList = document.querySelector('ul.post-list');
-    if (postList) {
-        var eachPost = postList.querySelectorAll('li');
+  const postList = document.querySelector("ul.post-list");
+  if (postList) {
+    var eachPost = postList.querySelectorAll("li");
 
-        var numberOfPosts = eachPost.length,
-            basisVal;
+    var numberOfPosts = eachPost.length,
+      basisVal;
 
-        for (var i = 0; i < numberOfPosts; i++) {
-            basisVal = randomIntFromInterval(20, 45);
-            eachPost[i].style['flex-basis'] = basisVal + '%';
+    for (var i = 0; i < numberOfPosts; i++) {
+      basisVal = randomIntFromInterval(20, 35);
+      eachPost[i].style["flex-basis"] = basisVal + "%";
 
-            //seta a cor da borda de topo de cada post
-            eachPost[i].style.borderColor = get_random_color();
-            eachPost[i].querySelector('h2').style.color = eachPost[i].style.borderTopColor
+      //seta a cor da borda de topo de cada post
+      eachPost[i].firstElementChild.style.borderTopColor = get_random_color();
+      eachPost[i].querySelector("h2").style.color =
+        eachPost[i].firstElementChild.style.borderTopColor;
 
-            eachPost[i].firstElementChild.addEventListener("mouseleave", function (e) {
-                this.style.borderBottomColor = 'transparent';
-            });
-            eachPost[i].firstElementChild.addEventListener("mouseenter", function () {
-                this.style.borderBottomColor = this.parentNode.style.borderTopColor;
-            });
-        }
+      eachPost[i].firstElementChild.addEventListener("mouseleave", function(e) {
+        this.style.borderBottomColor = "transparent";
+      });
+      eachPost[i].firstElementChild.addEventListener("mouseenter", function() {
+        this.style.borderBottomColor = this.style.borderTopColor;
+      });
     }
+  }
 }
 
 //método para gerar um número inteiro em determinado intervalo
 function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 //métodos para gerar cores aleatórias
 function rand(min, max) {
-    return parseInt(Math.random() * (max - min + 1), 10) + min;
+  return parseInt(Math.random() * (max - min + 1), 10) + min;
 }
 
 function get_random_color() {
-    var h = rand(1, 300); // color hue between 1 and 360
-    var s = rand(30, 50); // saturation 30-100%
-    var l = rand(45, 55); // lightness 30-70%
-    return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+  var h = rand(1, 300); // color hue between 1 and 360
+  var s = rand(30, 50); // saturation 30-100%
+  var l = rand(45, 55); // lightness 30-70%
+  return "hsl(" + h + "," + s + "%," + l + "%)";
 }
 
 /*
@@ -67,71 +68,94 @@ btnMenuMobile.addEventListener("click", function () {
 });
 */
 
-$('body').on('click', '.btn-toggle-menu', function () {
-    $(this).toggleClass(function () {
-        return $(this).is('.menu-open, .menu-close') ? 'menu-open menu-close' : 'menu-open';
-    });
+$("body").on("click", ".btn-toggle-menu", function() {
+  $(this).toggleClass(function() {
+    return $(this).is(".menu-open, .menu-close")
+      ? "menu-open menu-close"
+      : "menu-open";
+  });
 
-    $('#menuMobile').toggle();
-    $('.overlay-menu-mobile').toggle();
+  $("#menuMobile").toggle();
+  $(".overlay-menu-mobile").toggle();
 });
 
-$(window).on('resize', function () {
-    const el = document.getElementById("menuMobile");
-    // Caso o menu esteja visível durante um window resize
-    if (el.offsetParent !== null){
-        $('.btn-toggle-menu').toggleClass(function () {
-            return $(this).is('.menu-open, .menu-close') ? 'menu-open menu-close' : 'menu-open';
-        });
-        el.style.display = 'none';
-        $('.overlay-menu-mobile').hide();
-    } 
+$(window).on("resize", function() {
+  const el = document.getElementById("menuMobile");
+  // Caso o menu esteja visível durante um window resize
+  if (el.offsetParent !== null) {
+    $(".btn-toggle-menu").toggleClass(function() {
+      return $(this).is(".menu-open, .menu-close")
+        ? "menu-open menu-close"
+        : "menu-open";
+    });
+    el.style.display = "none";
+    $(".overlay-menu-mobile").hide();
+  }
 });
 
 //rotina para exibir minha foto atual como easter egg
 function ClickMeStuff() {
-    return this.clicks = 0;
+  return (this.clicks = 0);
 }
 
 function clickMeEvent() {
-    var el = document.getElementById("svgToShowMe"),
-        myselfChild = document.getElementById("meChild"),
-        myselfNow = document.getElementById("meNow"),
-        clickHandle = new ClickMeStuff();
+  var el = document.getElementById("svgToShowMe"),
+    myselfChild = document.getElementById("meChild"),
+    myselfNow = document.getElementById("meNow"),
+    clickHandle = new ClickMeStuff();
 
-    el.addEventListener("click", registerClicks, false);
+  el.addEventListener("click", registerClicks, false);
 
-    function registerClicks() {
-        clickHandle.clicks += 1;
-        if (clickHandle.clicks === 2) {
-            myselfChild.classList.add('me-child-out');
-            setTimeout(function () {
-                myselfChild.classList.add('d-none');
-            }, 550);
+  function registerClicks() {
+    clickHandle.clicks += 1;
+    if (clickHandle.clicks === 2) {
+      myselfChild.classList.add("me-child-out");
+      setTimeout(function() {
+        myselfChild.classList.add("d-none");
+      }, 550);
 
-            myselfNow.classList.add('me-now-in');
-            myselfNow.classList.remove('d-none');
+      myselfNow.classList.add("me-now-in");
+      myselfNow.classList.remove("d-none");
 
-            setTimeout(function () {
-                myselfChild.classList.add('me-child-in');
-                myselfChild.classList.remove('me-child-out', 'd-none');
+      setTimeout(function() {
+        myselfChild.classList.add("me-child-in");
+        myselfChild.classList.remove("me-child-out", "d-none");
 
-                myselfNow.classList.add('me-now-out');
-                myselfNow.classList.remove('me-now-in');
-                setTimeout(function () {
-                    myselfNow.classList.add('d-none');
-                    myselfNow.classList.remove('me-now-out');
-                    myselfChild.classList.remove('me-child-in');
-                }, 700);
+        myselfNow.classList.add("me-now-out");
+        myselfNow.classList.remove("me-now-in");
+        setTimeout(function() {
+          myselfNow.classList.add("d-none");
+          myselfNow.classList.remove("me-now-out");
+          myselfChild.classList.remove("me-child-in");
+        }, 700);
 
-                clickHandle.clicks = 0;
+        clickHandle.clicks = 0;
+      }, 10000);
 
-            }, 10000);
-
-            //console.log('--------------------------------------> ' + clickHandle.clicks);
-        }
+      //console.log('--------------------------------------> ' + clickHandle.clicks);
     }
+  }
 }
 //FIM rotina para exibir minha foto atual como easter egg
+
+// function rescroll() {
+//   $(".each-post-li").show();
+//   // hide everything that is out of bound
+//   $(".each-post-li")
+//     .filter(function(index) {
+//     //   console.log(
+//     //     $(this).position().top,
+//     //     $(window).height() + $(window).scrollTop()
+//     //   );
+//       return (
+//         $(this).position().top > $(window).height() + $(window).scrollTop()
+//       );
+//     })
+//     .hide();
+// }
+
+// $(window).scroll(function() {
+//   rescroll();
+// });
 
 //document.addEventListener("DOMContentLoaded", clickMeEvent, false);
